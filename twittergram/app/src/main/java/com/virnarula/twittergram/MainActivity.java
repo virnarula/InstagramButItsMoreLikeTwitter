0fd622bf21418849d062f0c5b24d6bff20b8467f
virnarula <virsvirus@gmail.com>
2020-05-26 22:08:14 -0500

    Create Android studio files

---
+++ twittergram/app/src/main/java/com/virnarula/twittergram/MainActivity.java
@@ -0,0 +1,16 @@
+package com.virnarula.twittergram;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import com.parse.Parse;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
